3dedf90c9c7185b4dbd3494281b954fb9a983c34
Raphael <raphael.maringer@student.htldornbirn.at>
2024-10-25 10:37:41 +0200

    finished 01-JSVariablen

---
+++ JS/01-JSVariablen/script.js
@@ -0,0 +1,45 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert " + a);
+
+console.log("a + b = " + c);
+
+// Rechnungsarten
+// *, /, +, -
+
+c = b - a;
+console.log("b + a = " + c);
+
+// Rechnung 1
+let result = a + b + c;
+console.log("Ergebnis: a + b + c = " + result);
+
+// Rechnung 2
+result = (a + b) / c
+console.log("Ergebnis:(a + b) / c = " + result);
+
+// Rechnung 3
+result = c - a + b;
+console.log("Ergebnis: c - a + b = " + result);
+
+// Rechnung 4
+result = a + b + c * 3;
+console.log("Ergebnis: a + b + c * 3 = " + result);
+
+// Rechnung 5
+result = (a + b + c) / 4;
+console.log("Ergebnis: (a + b + c) / 4 = " + result);
+
+a = 3;
+b = a + 3;
+c = c - 3;
+
+console.log("a hat den Wert " + a);
+console.log("b hat den Wert " + b);
+console.log("c hat den Wert " + c);
