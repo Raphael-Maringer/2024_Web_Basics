97f47f2052881abd475f458a970504d824c67b1c
Raphael <raphael.maringer@student.htldornbirn.at>
2024-11-04 15:57:44 +0100

    finished 02-JSStrings

---
+++ JS/02-JSStrings/script.js
@@ -0,0 +1,21 @@
+let a = 7;
+let b = 8;
+
+let ab = a - b;
+
+console.log(ab);
+
+// +, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+// Texte STRINGS
+
+let name = "Raphael";
+console.log(name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + result);
