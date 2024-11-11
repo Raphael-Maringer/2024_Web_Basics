5c65c83e6fe2db0e175a45a098b716e31877fcee
Raphael <raphael.maringer@student.htldornbirn.at>
2024-11-11 15:47:48 +0100

    finished 04-Loops

---
+++ JS/04-Loops/script.js
@@ -0,0 +1,71 @@
+console.log("test");
+
+// Schleife
+
+for(let count = 10; count >= 0; count--) {
+    console.log(count + " Raphael");
+}
+
+console.log("Fertig");
+
+// Zähle von 7 bis 45
+console.log("Aufgabe 1: ");
+
+for (let count = 7; count <= 45; count++) {
+    console.log(count);
+}
+
+console.log("-------------------");
+
+
+// Zähle von 9 bis 99
+console.log("Aufgabe 2: ");
+
+for (let count = 9; count <= 99; count++) {
+    console.log(count);
+}
+
+console.log("-------------------");
+
+
+// Zähle rückwärts von 10 bis 0
+console.log("Aufgabe 3: ");
+
+for (let count = 11; count >=0; count--) {
+    console.log(count);
+}
+
+console.log("-------------------");
+
+
+// Zähle rückwärts von 1234 bis 1207
+console.log("Aufgabe 4: ");
+
+for (let count = 1234; count >= 1207; count--) {
+    console.log(count);
+}
+
+console.log("-------------------");
+
+
+// Gib Deinen Namen genau 8 mal aus
+console.log("Aufgabe 5: ");
+
+for (let count = 1; count <= 8; count++) {
+    console.log("Raphael");
+}
+
+console.log("-------------------");
+
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+console.log("Aufgabe 6: ");
+for (let count = 0; count < 10; count++) {
+    console.log(count + 1);
+}
+
+console.log("Raphael");
+
+for (let count = 10; count >= 0; count--) {
+    console.log(count);
+}
