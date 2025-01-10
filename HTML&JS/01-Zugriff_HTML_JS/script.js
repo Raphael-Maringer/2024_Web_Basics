8b3d3cce54ebaab28b4dac620ded0e27a3ac6ea8
Raphael <raphael.maringer@student.htldornbirn.at>
2025-01-10 13:03:04 +0100

    finished 01-Zugriff_HTML_JS

--- HTML&JS/01-Zugriff_HTML_JS/script.js
+++ HTML&JS/01-Zugriff_HTML_JS/script.js
@@ -1,2 +1,42 @@
-let element = document.getElementById("content");
-console.log(element);
+function calculateSum() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    
+    let result = parseFloat(num1) + parseFloat(num2);
+
+    console.log(result);
+}
+
+function calculateDiff() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    
+    let result = parseFloat(num1) - parseFloat(num2);
+
+    console.log(result);
+}
+
+function calculatePrd() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    
+    let result = parseFloat(num1) * parseFloat(num2);
+
+    console.log(result);
+}
+
+function calculateQuot() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    
+    let result = parseFloat(num1) / parseFloat(num2);
+
+    console.log(result);
+}
+
+
+
