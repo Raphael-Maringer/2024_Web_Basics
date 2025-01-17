deb11412b20b9a56717af28d381151f2c2fedda3
Raphael <raphael.maringer@student.htldornbirn.at>
2025-01-17 10:32:41 +0100

    finished 02-RandomNumberGenerator

--- HTML&JS/02-RandomNumberGenerator/script.js
+++ HTML&JS/02-RandomNumberGenerator/script.js
@@ -1,8 +1,10 @@
 function generate() {
     let number1 = document.getElementById('field1').value
     let number2 = document.getElementById('field2').value
-    console.log(number1 + "-" + number2);
-    console.log("clicked");
-    let randomNumber = Math.floor(Math.random() * 10);
-    console.log(randomNumber);
+
+    let amountOfRandomnumbers = parseInt(number2) - parseInt(number1) + 1;
+    // console.log(number1 + "-" + number2);
+
+    let randomNumber = Math.floor(Math.random() * amountOfRandomnumbers) + parseInt(number1);
+    document.getElementById("randomNumber").innerHTML = randomNumber;
 }
