ef4c90987fb4a85b23c348f37e4597b228a0030b
Raphael <raphael.maringer@student.htldornbirn.at>
2025-01-27 16:40:17 +0100

    finished 03-JSON

---
+++ HTML&JS/03-JSON/script.js
@@ -0,0 +1,17 @@
+let data = [
+    {firstName:"Hans", lastName:"Huber", points: 25},
+    {firstName:"Paul", lastName:"Müller", points: 30},
+    {firstName:"Franz", lastName:"Maier", points: 35},
+]
+
+
+function loadPeople() {
+    let html = "";
+    data.forEach(element => {
+        html += "<div>" + element.firstName + " " + element.lastName + "<div>" 
+    });
+    document.getElementById("context").innerHTML = html;
+    
+}
+
+loadPeople();
