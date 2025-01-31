d75bfcf7ee40d3f94172d28407a7aaecf187c125
Raphael <raphael.maringer@student.htldornbirn.at>
2025-01-31 09:19:54 +0100

    finished loop-exercise

--- Studying/script3.js
+++ Studying/script3.js
@@ -12,3 +12,28 @@ for (let i = 7; i < 344; i++) {
 }
 console.log(result);
 
+
+// Aufgabe 2
+let data = "Super Data".split("");
+let output = "";
+
+for (let i = 0; i < data.length; i++) {
+    if (data[i] == "u" || data[i]== "e"){
+        output += "x";
+    } else {
+        output += data[i];
+    }
+}
+
+console.log(output);
+
+
+// Aufgabe 3
+let num= 100;
+
+while (num >= 25) {
+    if (num !== 50 && num !== 40 && num !== 30) {
+        console.log(num);
+    }
+    num -= 5;
+}
