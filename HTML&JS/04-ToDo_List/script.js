0df2e5ac34073cd05778223cb16fdd368bf1e566
Raphael <raphael.maringer@student.htldornbirn.at>
2025-02-07 09:56:44 +0100

    working on 04-ToDo_List

---
+++ HTML&JS/04-ToDo_List/script.js
@@ -0,0 +1,14 @@
+let todos  = [
+    {id:1, title:"putzen", isDone:false},
+    {id:2, title:"kochen", isDone:true},
+    {id:3, title:"einkaufen", isDone:false},
+    {id:4, title:"lernen", isDone:false}
+]
+
+let html = "";
+todos.forEach(element => {
+    console.log(element.title);
+    html+="<div>" + element.title + " - " + element.isDone + "</div>";
+});
+
+document.getElementById("todos").innerHTML = html;
