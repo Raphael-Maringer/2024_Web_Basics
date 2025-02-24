ba3a049e65d8a8e1675eb0019812178e2f17ecf5
Raphael <raphael.maringer@student.htldornbirn.at>
2025-02-24 16:10:27 +0100

    finished 04-ToDo_List

--- HTML&JS/04-ToDo_List/script.js
+++ HTML&JS/04-ToDo_List/script.js
@@ -5,9 +5,45 @@ let todos  = [
     {id:4, title:"lernen", isDone:false}
 ];
 
-let html = "";
-todos.forEach((element) => {
-    html+="<div>" + element.title + " - " + element.isDone + "</div>";
-});
+let maxID=4;
+printTasks();
 
-document.getElementById("todos").innerHTML = html;
+document.getElementById("input").addEventListener("change",function(event){
+    console.log(event.target.value);
+    maxID++;
+    todos.push({id:maxID, title:event.target.value, isDone:false})
+    printTasks();
+    event.target.value = ""
+})
+
+
+
+function printTasks(){
+    let html = "";
+    todos.forEach((element) => {
+        let style = element.isDone?"green":"none";
+        html+="<div class='task' style='background-color:" + style + "'>" + element.title + " - " + element.isDone + "<div onClick='setIsDone(" + element.id + ")'>Done</div><div onClick='deleteTask(" + element.id + ")'>Delete</div></div>";
+    });
+    
+    document.getElementById("todos").innerHTML = html;
+}
+
+function deleteTask(id) {
+    let index = todos.findIndex(element=> {
+        if (element.id === id) {
+            return true;
+        }
+    })
+    todos.splice(index, 1);
+    printTasks();
+}
+
+function setIsDone(id) {
+    let todo = todos.find(element=> {
+        if (element.id === id) {
+            return true;
+        }
+    })
+    todo.isDone = !todo.isDone;
+    printTasks();
+}
