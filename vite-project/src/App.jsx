11e34e1006d5fa6012e2fb1bbc7c55b6f0e86558
Raphael <raphael.maringer@student.htldornbirn.at>
2025-03-14 10:25:18 +0100

    working on 02-Components: finished List

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Card from './components/Card'
+import List from './components/List'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,8 @@ function App() {
       <h1 className="bg-amber-300 p-4 text-center">Hello</h1>
       <div className="flex">
       <Card name={"Lenz"} jobtitle={"AV"} description={"lässig"}/>
-      <Card name={"Sams"} jobtitle={"AV"} description={"auch lässig"}/> </div>     
+      <Card name={"Sams"} jobtitle={"AV"} description={"auch lässig"}/> </div> 
+      <List drink1={"Coffee"} drink2={"Tea"} drink3={"Beer"} ></List>    
     </>
   )
 }
---
+++ vite-project/src/components/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function List({drink1, drink2, drink3}) {
+  return (
+    <div className="bg-cyan-800 w-[200px] h-[80px] m-[30px] p-[5px]">
+        <p className="border-b-2 border-white">{drink1}</p>
+        <p className="border-b-2 border-white">{drink2}</p>
+        <p className="border-b-2 border-white">{drink3}</p>
+    </div>
+  )
+}
