939df4361be078212ab9c9dc0a242b06b880a08c
Raphael <raphael.maringer@student.htldornbirn.at>
2025-03-24 16:03:07 +0100

    Refactored filename Exercise1 (note: file includes exercise 1 & 2)

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Exercise1 from './components/pages/Komponenten'
+import Exercise1 from './components/pages/Exercise1'
 
 function App() {
   const [count, setCount] = useState(0)
