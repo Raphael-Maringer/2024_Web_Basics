70dbda9e0ac29638e75111674c3299d754211f98
Raphael <raphael.maringer@student.htldornbirn.at>
2025-04-04 09:35:00 +0200

    finished 04-State_useEffect

--- React/src/components/Peoplecontainer.jsx
+++ React/src/components/Peoplecontainer.jsx
@@ -4,6 +4,7 @@ import Personcard from './Personcard'
 
 export default function Peoplecontainer() {
   const [people, setPeople] = useState([]);
+  const [color, setColor] = useState("green");
 
   useEffect(()=>{
     fetch("https://67b847d0699a8a7baef366ee.mockapi.io/person").then(
@@ -13,9 +14,14 @@ export default function Peoplecontainer() {
     );
   }, []);
 
+  useEffect(() => {
+    alert("change");
+  }, [color]);
+
   return (
     <div>
         <h1>People</h1>
+        <button onClick={() => setColor("orange")}>Change Color</button>
         <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
           {people.map(person => {
             return <Personcard name={person.name} title={person.jobTitle} imageUri={person.avatar}></Personcard>
