f4f62e4dc3f5178cd883bbcd6d5da21b98cb7c93
Raphael <raphael.maringer@student.htldornbirn.at>
2025-05-09 10:39:44 +0200

    finished 05-Filter

--- React/src/components/Peoplecontainer.jsx
+++ React/src/components/Peoplecontainer.jsx
@@ -21,21 +21,20 @@ export default function Peoplecontainer() {
   }, [color]);
 
   const filterPeople = (filter) => {
-    let filtered = people.filter(person => person.name.includes(filter));
+    let filtered = people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
     setFilteredPeople(filtered)
   }
 
   return (
-    <div>
-        <h1>People</h1>
-        <div className='border'>
+    <div className=''>
+        <div className='fixed border mb-4 bg-white w-full h-28 p-8'>
           <input className='border p-3' type="text" placeholder="Search" onChange={(el) =>{
             console.log(el.target.value)
             filterPeople(el.target.value)
           }}/>
         </div>
         <button onClick={() => setColor("orange")}>Change Color</button>
-        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32'>
           {filteredPeople.map(person => {
             return <Personcard name={person.name} title={person.jobTitle} imageUri={person.avatar}></Personcard>
           })}
